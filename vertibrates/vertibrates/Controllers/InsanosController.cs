c3941d3f94987f2b3da3ab85acd7817ad7e7ed69
samsad <samali2md@gmail.com>
2021-10-06 20:46:34 +0545

    insanos

---
+++ vertibrates/vertibrates/Controllers/InsanosController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vertibrates.Models;
+using System.Data.Entity;
+
+namespace vertibrates.Controllers
+{
+    public class InsanosController : Controller
+    {
+        MainEntities mainEntity = new MainEntities();
+        // GET: Insanos
+        public ActionResult Index()
+        {
+            var list = mainEntity.project_table.ToList();
+            return View(list);
+        }
+        private void CommonDropDown()
+        {
+            ViewBag.ProjetList = new SelectList(mainEntity.project_table.ToList(), "id", "name");
+
+        }
+        public ActionResult AddNew()
+        {
+            CommonDropDown();
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult AddNew(project_table project_table)
+        {
+            mainEntity.project_table.Add(project_table);
+            mainEntity.SaveChanges();
+            ViewBag.Message = "Saved Successful";
+            CommonDropDown();
+            return View();
+
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            project_table project_Table = mainEntity.project_table.Find(id);
+            CommonDropDown();
+            return View(project_Table);
+        }
+      
+      [HttpPost]
+      public ActionResult Edit(project_table project_Table)
+        {
+            mainEntity.Entry(project_Table).State = EntityState.Modified;
+            mainEntity.SaveChanges();
+            ViewBag.Message = "Updated Successful";
+            CommonDropDown();
+            return View(project_Table);
+
+        }
+        public ActionResult Delete(int id)
+        {
+            project_table project_Table = mainEntity.project_table.Find(id);
+            return View(project_Table);
+        }
+
+        [HttpPost]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            project_table old_Data = mainEntity.project_table.Find(id);
+            mainEntity.project_table.Remove(old_Data);
+            mainEntity.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ vertibrates/vertibrates/Models/InsaanClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace vertibrates.Models
+{
+    public class InsaanClass
+    {
+        public string Name {set; get;}
+        public string Address {set; get;}
+        public string Contact {set; get;}
+        public bool Test { set; get; }
+    }
+}
